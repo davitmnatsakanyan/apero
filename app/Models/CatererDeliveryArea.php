2605737a6503960fe6a80b8e4cbd5aa587e6c3e8
Sona <sona.khachatryan1995@gmail.com>
2016-07-06 18:07:31 +0400

    caterer management2

---
+++ app/Models/CatererDeliveryArea.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CatererDeliveryArea extends Model
+{
+
+    protected $fillable = ['caterer_id' , 'zip_code_id'];
+
+
+}
