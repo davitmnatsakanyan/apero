904b0330e55b07a95a35c7d9fd19f4d6f9bc0653
Sona <sona.khachatryan1995@gmail.com>
2016-07-11 19:06:19 +0400

    hbhug

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Guest;
+use App\Models\Order;
 use App\Http\Controllers\PaypalController;
 use Auth, View;
 
