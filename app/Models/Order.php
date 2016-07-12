16450b0260cbd6a3c7443bea80801da9c4cefed0
Sona <sona.khachatryan1995@gmail.com>
2016-07-12 13:13:34 +0400

    database changes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+   protected $fillable = [
+       'user_id',
+       'caterer_id',
+       'delivery_address',
+       'delivery_zip',
+       'delivery_city',
+       'delivery_country',
+       'email',
+       'phone',
+       'mobile',
+       'payment_type',
+       'status',
+       'total_cost',
+       'remember_token',
+       'billing_address'
+   ];
 }
--- database/migrations/2016_07_01_082109_update_caterers_table2.php
+++ database/migrations/2016_07_01_082109_update_caterers_table2.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateCaterersTable extends Migration
+class UpdateCaterersTable2 extends Migration
 {
     /**
      * Run the migrations.
